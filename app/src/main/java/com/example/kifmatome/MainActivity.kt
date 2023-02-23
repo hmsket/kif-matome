a938d2f7eae468a1d232041ee21805abdf9719ee
hmsket <ryosuke1257@gmail.com>
2023-02-24 01:50:50 +0900

    add: オプションメニューを作成

--- app/src/main/java/com/example/kifmatome/MainActivity.kt
+++ app/src/main/java/com/example/kifmatome/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.kifmatome
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.option_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // when (item.itemId) {
+        //     R.id.add_tab -> 処理
+        //     R.id.add_kif ->
+        //     R.id.delete_tab ->
+        //     R.id.delete_tab ->
+        //     R.id.sort_tab ->
+        //   R.id.sort_tab ->
+        // }
+        return true
     }
 }
